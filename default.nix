05a20bc9847751a61224473a91262e97e12eb02e
David Mkhitaryan <davtot111@icloud.com>
2026-03-22 12:27:07 +0400

    Included missing package entry for 'notion-river' in the module + default.nix

--- default.nix
+++ default.nix
@@ -2,20 +2,22 @@
   nixosModule = import ./river-module.nix;
 
   packages = {
-    river-next  = ./river-dev.nix;
-    beansprout  = ./window-managers/beansprout/package.nix;
-    canoe       = ./window-managers/canoe/package.nix;
-    kuskokwim   = ./window-managers/kuskokwim/package.nix;
-    kwm         = ./window-managers/kwm/package.nix;
-    machi       = ./window-managers/machi/package.nix;
-    mousetrap   = ./window-managers/mousetrap/package.nix;
-    orilla      = ./window-managers/orilla/package.nix;
-    pwm         = ./window-managers/pwm/package.nix;
-    rhine       = ./window-managers/rhine/package.nix;
-    rijan       = ./window-managers/rijan/package.nix;
-    rill        = ./window-managers/rill/package.nix;
-    rrwm        = ./window-managers/rrwm/package.nix;
-    tarazed     = ./window-managers/tarazed/package.nix;
-    zrwm        = ./window-managers/zrwm/package.nix;
+    river-next = ./river-dev.nix;
+    beansprout = ./window-managers/beansprout/package.nix;
+    canoe = ./window-managers/canoe/package.nix;
+    kuskokwim = ./window-managers/kuskokwim/package.nix;
+    kwm = ./window-managers/kwm/package.nix;
+    machi = ./window-managers/machi/package.nix;
+    mousetrap = ./window-managers/mousetrap/package.nix;
+    notion-river = ./window-managers/notion-river/package.nix;
+    orilla = ./window-managers/orilla/package.nix;
+    pwm = ./window-managers/pwm/package.nix;
+    rhine = ./window-managers/rhine/package.nix;
+    rijan = ./window-managers/rijan/package.nix;
+    rill = ./window-managers/rill/package.nix;
+    rrwm = ./window-managers/rrwm/package.nix;
+    tarazed = ./window-managers/tarazed/package.nix;
+    zrwm = ./window-managers/zrwm/package.nix;
+    reka = ./window-managers/reka/package.nix;
   };
 }
--- river-module.nix
+++ river-module.nix
@@ -21,6 +21,7 @@ let
     kwm = pkgs.callPackage ./window-managers/kwm/package.nix { };
     machi = pkgs.callPackage ./window-managers/machi/package.nix { };
     mousetrap = pkgs.callPackage ./window-managers/mousetrap/package.nix { };
+    notion-river = pkgs.callPackage ./window-managers/notion-river/package.nix { };
     orilla = pkgs.callPackage ./window-managers/orilla/package.nix { };
     pwm = pkgs.callPackage ./window-managers/pwm/package.nix { };
     rhine = pkgs.callPackage ./window-managers/rhine/package.nix { };
@@ -79,6 +80,7 @@ in
         "kwm"
         "machi"
         "mousetrap"
+        "notion-river"
         "orilla"
         "pwm"
         "reka"
